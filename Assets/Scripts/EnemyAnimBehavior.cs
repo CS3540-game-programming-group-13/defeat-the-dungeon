782bf4f2387a582815801828e47595dd8e6727a9
rjiang910 <83145606+rjiang910@users.noreply.github.com>
2023-03-19 19:04:43 -0400

    Created Goblin NPC Behavior
    Goblin can wander to and from four points randomly.
    At a certain distance the goblin will chase the player when he approaches and attack at a certain distance.
    The player can kill the goblin with his sword with one hit. (We can change this to make it harder if we want)
    Patrol, Chase, Attack, and Death Animations

---
+++ Assets/Scripts/EnemyAnimBehavior.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimBehavior : MonoBehaviour
+{
+    public enum FSMStates
+    {
+        Idle,
+        Patrol,
+        Chase,
+        Attack,
+        Dead
+    }
+
+    public FSMStates currentState;
+
+    public float attackDistance = 2;
+    public float chaseDistance = 6;
+    public float enemySpeed = 5;
+    public GameObject player;
+    public AudioClip goblinDeathSFX;
+
+
+    GameObject[] wanderPoints;
+    Vector3 nextDestination;
+    Animator anim;
+    float distanceToPlayer;
+    float elapsedTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wanderPoints = GameObject.FindGameObjectsWithTag("WanderPoint");
+
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        switch (currentState)
+        {
+            case FSMStates.Patrol:
+                UpdatePatrolState();
+                break;
+            case FSMStates.Chase:
+                UpdateChaseState();
+                break;
+            case FSMStates.Attack:
+                UpdateAttackState();
+                break;
+            case FSMStates.Dead:
+                UpdateDeadState();
+                break;
+        }
+
+        elapsedTime += Time.deltaTime;
+    }
+
+    private void Initialize()
+    {
+        currentState = FSMStates.Patrol;
+        FindNextPoint();
+    }
+
+    void UpdatePatrolState()
+    {
+        print("Patrolling!");
+
+        anim.SetInteger("goblinState", 1);
+
+        if (Vector3.Distance(transform.position, nextDestination) < 3)
+        {
+            FindNextPoint();
+        }
+        else if (distanceToPlayer <= chaseDistance)
+        {
+            currentState = FSMStates.Chase;
+        }
+
+        FaceTarget(nextDestination);
+
+        /*
+        transform.position = Vector3.MoveTowards
+            (transform.position, nextDestination, enemySpeed * Time.deltaTime);*/
+
+    }
+    void UpdateChaseState()
+    {
+        print("Chasing!");
+
+        anim.SetInteger("goblinState", 2);
+
+        nextDestination = player.transform.position;
+
+        if (distanceToPlayer <= attackDistance)
+        {
+            currentState = FSMStates.Attack;
+        }
+        else if (distanceToPlayer > chaseDistance)
+        {
+            currentState = FSMStates.Patrol;
+        }
+
+        FaceTarget(nextDestination);
+
+        /*
+        transform.position = Vector3.MoveTowards
+            (transform.position, nextDestination, enemySpeed * Time.deltaTime);
+        */
+    }
+    void UpdateAttackState()
+    {
+        print("Attacking!");
+
+        nextDestination = player.transform.position;
+
+        if (distanceToPlayer <= attackDistance)
+        {
+            currentState = FSMStates.Attack;
+        }
+        else if (distanceToPlayer > attackDistance && distanceToPlayer <= chaseDistance)
+        {
+            currentState = FSMStates.Chase;
+        }
+        else if (distanceToPlayer > chaseDistance)
+        {
+            currentState = FSMStates.Patrol;
+        }
+
+        FaceTarget(nextDestination);
+
+        anim.SetInteger("goblinState", 3);
+
+    }
+    void UpdateDeadState()
+    {
+        print("Died");
+
+        anim.SetInteger("goblinState", 4);
+
+        AudioSource.PlayClipAtPoint(goblinDeathSFX, transform.position);
+
+        Destroy(gameObject, anim.GetCurrentAnimatorStateInfo(0).length);
+
+        currentState = FSMStates.Dead;
+    }
+
+    void FindNextPoint()
+    {
+        int randomIndex = Random.Range(0, wanderPoints.Length);
+        nextDestination = wanderPoints[randomIndex].transform.position;
+    }
+
+    void FaceTarget(Vector3 target)
+    {
+        Vector3 directionToTarget = (target - transform.position).normalized;
+        directionToTarget.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
+        transform.rotation = Quaternion.Slerp
+            (transform.rotation, lookRotation, 10 * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //attack
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Sword"))
+        {
+            UpdateDeadState();
+        }
+    }
+}
